31e1b57943f90d86f1e6072334838d7c1f1f2b2c
Sai-Chowdary393 <saic6832@gmail.com>
2026-09-07 11:33:33 +0100

    Update Sidebar.jsx

--- components/layout/Sidebar.jsx
+++ components/layout/Sidebar.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+
 import {
   usePathname,
 } from "next/navigation";
@@ -177,6 +178,29 @@ const navigationGroups = [
 
         icon:
           "◷",
+
+        modules: [
+          "Follow-ups",
+          "Follow Ups",
+          "Tasks",
+        ],
+
+        prefixes: [
+          "followups.",
+          "follow-ups.",
+          "tasks.",
+        ],
+      },
+
+      {
+        label:
+          "Calendar",
+
+        href:
+          "/calendar",
+
+        icon:
+          "▦",
 
         modules: [
           "Follow-ups",
